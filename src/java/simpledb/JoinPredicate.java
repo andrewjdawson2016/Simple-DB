7c26268344caed774f744f1056f8ca53d5116692
Andrew Dawson <andrewjdawson2016@gmail.com>
2016-04-13 21:14:23 -0700

    finished JoinPredicate

--- src/java/simpledb/JoinPredicate.java
+++ src/java/simpledb/JoinPredicate.java
@@ -10,6 +10,10 @@ public class JoinPredicate implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private int field1;
+    private Predicate.Op op;
+    private int field2;
+    
     /**
      * Constructor -- create a new predicate over two fields of two tuples.
      * 
@@ -25,7 +29,9 @@ public class JoinPredicate implements Serializable {
      * @see Predicate
      */
     public JoinPredicate(int field1, Predicate.Op op, int field2) {
-        // some code goes here
+        this.field1 = field1;
+        this.op = op;
+        this.field2 = field2;
     }
 
     /**
@@ -35,25 +41,18 @@ public class JoinPredicate implements Serializable {
      * @return true if the tuples satisfy the predicate.
      */
     public boolean filter(Tuple t1, Tuple t2) {
-        // some code goes here
-        return false;
+        return t1.getField(this.field1).compare(this.op, t2.getField(this.field2));
     }
     
-    public int getField1()
-    {
-        // some code goes here
-        return -1;
+    public int getField1() {
+        return this.field1;
     }
     
-    public int getField2()
-    {
-        // some code goes here
-        return -1;
+    public int getField2() {
+    	return this.field2;
     }
     
-    public Predicate.Op getOperator()
-    {
-        // some code goes here
-        return null;
+    public Predicate.Op getOperator() {
+    	return this.op;
     }
 }
