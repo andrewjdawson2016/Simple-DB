374e88ba500a2ac3bb030052f937dcbcfe2debbf
Andrew Dawson <andrewjdawson2016@gmail.com>
2016-06-05 12:44:05 -0700

    finished first draft of ShuffleProducer

--- src/java/simpledb/parallel/ShuffleProducer.java
+++ src/java/simpledb/parallel/ShuffleProducer.java
@@ -1,5 +1,11 @@
 package simpledb.parallel;
 
+import java.util.ArrayList;
+
+import org.apache.mina.core.future.IoFutureListener;
+import org.apache.mina.core.future.WriteFuture;
+import org.apache.mina.core.session.IoSession;
+
 import simpledb.DbException;
 import simpledb.DbIterator;
 import simpledb.TransactionAbortedException;
@@ -17,6 +23,8 @@ import simpledb.TupleDesc;
 public class ShuffleProducer extends Producer {
 
     private static final long serialVersionUID = 1L;
+    
+    private transient WorkingThread runningThread;
 
     private DbIterator child;
     private ParallelOperatorID operatorID;
@@ -48,21 +56,76 @@ public class ShuffleProducer extends Producer {
         return this.pf;
     }
 
-    // some code goes here
     class WorkingThread extends Thread {
         public void run() {
 
-            // some code goes here
+        	for (SocketInfo si : ShuffleProducer.this.workers) {
+        		
+        		IoSession session = ParallelUtility.createSession(si.getAddress(), ShuffleProducer.this.getThisWorker().minaHandler, -1);
+
+                try {
+                    ArrayList<Tuple> buffer = new ArrayList<Tuple>();
+                    long lastTime = System.currentTimeMillis();
+
+                    while (ShuffleProducer.this.child.hasNext()) {
+                        Tuple tup = ShuffleProducer.this.child.next();
+                        buffer.add(tup);
+                        int cnt = buffer.size();
+                        if (cnt >= TupleBag.MAX_SIZE) {
+                            session.write(new TupleBag(
+                            		ShuffleProducer.this.operatorID,
+                            		ShuffleProducer.this.getThisWorker().workerID,
+                                    buffer.toArray(new Tuple[] {}),
+                                    ShuffleProducer.this.getTupleDesc()));
+                            buffer.clear();
+                            lastTime = System.currentTimeMillis();
+                        }
+                        if (cnt >= TupleBag.MIN_SIZE) {
+                            long thisTime = System.currentTimeMillis();
+                            if (thisTime - lastTime > TupleBag.MAX_MS) {
+                                session.write(new TupleBag(
+                                		ShuffleProducer.this.operatorID,
+                                		ShuffleProducer.this.getThisWorker().workerID,
+                                        buffer.toArray(new Tuple[] {}),
+                                        ShuffleProducer.this.getTupleDesc()));
+                                buffer.clear();
+                                lastTime = thisTime;
+                            }
+                        }
+                    }
+                    if (buffer.size() > 0)
+                        session.write(new TupleBag(ShuffleProducer.this.operatorID,
+                        		ShuffleProducer.this.getThisWorker().workerID,
+                                buffer.toArray(new Tuple[] {}),
+                                ShuffleProducer.this.getTupleDesc()));
+                    session.write(new TupleBag(ShuffleProducer.this.operatorID,
+                    		ShuffleProducer.this.getThisWorker().workerID)).addListener(new IoFutureListener<WriteFuture>(){
+
+                                @Override
+                                public void operationComplete(WriteFuture future) {
+                                    ParallelUtility.closeSession(future.getSession());
+                                }});//.awaitUninterruptibly(); //wait until all the data have successfully transfered
+                } catch (DbException e) {
+                    e.printStackTrace();
+                } catch (TransactionAbortedException e) {
+                    e.printStackTrace();
+                }
+        	}
+            
         }
     }
 
     @Override
     public void open() throws DbException, TransactionAbortedException {
-        // some code goes here
+        this.child.open();
+        this.runningThread = new WorkingThread();
+        this.runningThread.start();
+        super.open();
     }
 
     public void close() {
-        // some code goes here
+        super.close();
+        child.close();
     }
 
     @Override
@@ -72,13 +135,16 @@ public class ShuffleProducer extends Producer {
 
     @Override
     public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
+    	return this.child.getTupleDesc();
     }
 
     @Override
     protected Tuple fetchNext() throws DbException, TransactionAbortedException {
-        // some code goes here
+        try {
+            runningThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
